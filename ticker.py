872f88e56e03c58c60ddea39478c385180d26b57
Amit Gupta <gupta1@adobe.com>
2017-04-07 09:23:34 +0530

    removed unnecessary stuff and orig ticker.py comments

--- ticker.py
+++ ticker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
-# Stock ticker display on the TechSummit 2017 badge
-# J. Peterson, March 2017
+# Smart Meeting Room using Adobe IOT Badge (Arduino Lily Pad USB based)
+# Thinking out of the cubicle(gupta1, vipaggar, thirani)
 #
 
 import re, string, time, urllib, os, sys, time
@@ -9,12 +9,10 @@ import csv, urllib, StringIO, serial
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
-# List of stocks you want to display (keep it <= 20)
-stocks = ['AAPL','GOOG','ADBE','MSFT','AMZN','TWTR','ADSK','VFINX','NVDA','TXN']
 
 # Where to open a connection to the badge
 # See https://wiki.corp.adobe.com/pages/viewpage.action?spaceKey=tech2017&title=How+to+Reuse+the+IoT+Badge
-badge_serial_port = 'COM3'
+badge_serial_port = 'COM4'
 
 class State:
 	FREE = 1
@@ -33,33 +31,6 @@ def debugPrint(str):
 	if mode == Mode.DEBUG:
 		sys.stdout.write(str)
 	
-class Quote(object):
-	def __init__(self,data):
-		# Order of the keys matches the nsl1c1 format
-		# from Yahoo CSV stock data
-		keys = ['name','sym','price','change']
-		for i in range(len(keys)):
-			self.__dict__[keys[i]] = data[i]
-
-	def dump(self):
-		print self.__dict__
-
-def loadQuotes():
-	# See http://www.jarloo.com/yahoo_finance/
-	# n=name, s=symbol, l1=last trade price, c6=change
-	infoRequest = 'nsl1c1'
-	reqURL = 'http://finance.yahoo.com/d/quotes.csv?s=' + string.join(stocks,'+') + '&f=' + infoRequest
-
-	csvReader = csv.reader(urllib.urlopen(reqURL))
-	stockDict = {}
-	for row in csvReader:
-		q = Quote(row)
-		stockDict[q.sym] = q
-
-##	for q in stockDict.keys():
-##		stockDict[q].dump()
-	return stockDict
-
 freeScreenMsg = ["1 to Book", "3 for Housekeeping", ""]
 bookingCurrentlyScreenMsg = [ "2 to add time", "3 to dec time", "4 to book"]
 bookedUnoccupiedScreenMsg = ["1 mails attendees", "3 for Housekeeping", "4 to Occupy"]
@@ -144,8 +115,6 @@ class MeetingRoom:
 		
 	def updateRoomStatus(self):
 		nop
-		#self.status = State.BOOKED_OCCUPIED
-		#self.statusTill = "0.5 hr"
 		
 	def getMeetingName(self):
 		return "Ice Cream Party"
@@ -156,11 +125,13 @@ class MeetingRoom:
 		
 room = MeetingRoom()
 room.setName('EKLAVYA')
+
 class ColorCode:
 	green = [ 0,30, 0]
 	red =   [30, 0, 0]
 	yellow =[30,15, 0]
 	blue  = [ 0, 0,30]
+
 class Badge:
 	def __init__(self):
 		self.port = serial.Serial(badge_serial_port, baudrate=9600,timeout=1)
@@ -302,7 +273,7 @@ class Badge:
 		elif room.state == State.BOOKED_OCCUPIED or room.state == State.BOOKED_UNOCCUPIED:
 			if room.state == State.BOOKED_OCCUPIED:
 				self.setBothLEDColor(ColorCode.red)
-				self.threeLineRoomStatusDisplay(room.name, "Booked", "Till %s hrs %s mins" % (str(room.bookedTill/60), str(room.bookedTill%60)))
+				self.threeLineRoomStatusDisplay(room.name, "Occupied", "Till %s hrs %s mins" % (str(room.bookedTill/60), str(room.bookedTill%60)))
 			else:
 				self.setBothLEDColor(ColorCode.yellow)
 				self.threeLineRoomStatusDisplay(room.name, "Booked", "Press 4 to occupy")			
